da29d615c306f032ac4dea83210dd41025517e5a
alexeden69 <alexeden69@gmail.com>
2026-07-19 19:52:50 +0200

    chore: prepare github pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Site-Tickets',
+  assetPrefix: '/Site-Tickets/',
 };
 
 module.exports = nextConfig;
